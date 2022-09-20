7e3b6783590a7e90fe8e796497788d9728716422
moaliyou <moaliyou47@gmail.com>
2022-09-20 08:36:28 +0000

    initialize

---
+++ Main.java
@@ -0,0 +1,13 @@
+package oops;
+
+//import classes.*;
+
+public class Main{
+
+    public static void main(String[] args){
+
+        System.out.println("Hello, CS50 welcome to your new codespace :).");
+
+    }
+
+}
